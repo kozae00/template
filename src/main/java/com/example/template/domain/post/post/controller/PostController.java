2255bc414656f005c60a999c4407a788d6cffe01
kozae00 <rhwogus777@gmail.com>
2025-01-23 14:15:25 +0900

    show write

---
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -0,0 +1,22 @@
+package com.example.template.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/posts") // 모든 요청에 /posts를 붙여줌
+public class PostController {
+    @GetMapping("/write")
+    @ResponseBody
+    public String showWrite() {
+        return """
+                <form action="/doWrite">
+                    <input type="text" name="title" placeholder="제목" />
+                    <textarea name = "content"></textarea>
+                    <input type = "submit" value="등록" />
+                </form>
+                """;
+    }
+}
