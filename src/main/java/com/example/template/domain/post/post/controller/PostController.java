54053b92fe4cffc8ac458703d65f05ecfc09efc8
kozae00 <rhwogus777@gmail.com>
2025-01-23 15:14:19 +0900

    validation upgrade

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -13,7 +13,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form method="post"> 
+                <form method="post">
                     <input type="text" name="title" placeholder="제목" />
                     <textarea name = "content"></textarea>
                     <input type = "submit" value="등록" />
@@ -26,12 +26,49 @@ public class PostController {
     public String doWrite(String title, String content) {
 
         if (title.isBlank() || title == null) {
-            return "제목을 입력해주세요";
+            return """
+                <div>%s</div>
+                <form method="post">
+                    <input type="text" name="title" placeholder="제목" />
+                    <textarea name = "content"></textarea>
+                    <input type = "submit" value="등록" />
+                </form>
+                """.formatted("제목을 입력해주세요");
         }
 
         if (content.isBlank() || content == null) {
-            return "내용을 입력해주세요";
+            return """
+                <div>%s</div>
+                <form method="post">
+                    <input type="text" name="title" placeholder="제목" />
+                    <textarea name = "content"></textarea>
+                    <input type = "submit" value="등록" />
+                </form>
+                """.formatted("내용을 입력해주세요");
         }
+
+        if (title.length() < 5) {
+            return """
+                <div>%s</div>
+                <form method="post">
+                    <input type="text" name="title" placeholder="제목" />
+                    <textarea name = "content"></textarea>
+                    <input type = "submit" value="등록" />
+                </form>
+                """.formatted("제목을 5글자 이상 입력해주세요");
+        }
+        
+        if(content.length() < 10) {
+            return """
+                <div>%s</div>
+                <form method="post">
+                    <input type="text" name="title" placeholder="제목" />
+                    <textarea name = "content"></textarea>
+                    <input type = "submit" value="등록" />
+                </form>
+                """.formatted("내용을 10글자 이상 입력해주세요");
+        }
+
             return """
                     <h1>게시물 조회</h1>
                     <div>%s</div>
